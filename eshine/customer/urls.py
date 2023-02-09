813e8190f79ae468f2281da69c5448c53cd5cb54
Atul Kumar Singh <atul0003214@gmail.com>
2023-02-09 16:48:34 +0530

    First Commit

---
+++ eshine/customer/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('',views.customer_details,name="customer_details"),
+    path('add-address/',views.add_address,name="add_address"),
+    path('update_address/',views.update_address,name="update_address"),
+    path('remove_address/',views.remove_address,name="remove_address"),
+]
---
+++ eshine/registration/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from customer.models import CustomerModel
+from django import forms
+
+class createUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        # fields = ('first_name','last_name','email','username','DateOfBirth')
+        fields = ('first_name','last_name','email','username')
+
+class createUserFormCustome(forms.ModelForm):
+    class Meta:
+        model = CustomerModel
+        fields = ('DateOfBirth',)
+        error_messages = {
+            'DateOfBirth':{"invalid":"Enter in this format YYYY-MM-DD"}
+        }
+        
+
+
+
